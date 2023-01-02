4498c2da765d547fb98f9869fe9f3981e6e51bff
robertoalvs <109154194+robertoalvs@users.noreply.github.com>
2023-01-02 11:52:46 -0300

    atualizado

---
+++ resumo-aula-11-23/index-2.js
@@ -0,0 +1,9 @@
+let país = 'Brasil'
+if(país == 'Brasil'){
+
+    console.log('Você é BRASILEIRO!!!')
+}
+else {
+
+    console.log('Você é ESTRANGEIRO!!!')
+}
---
+++ resumo-aula-11-23/index.js
@@ -0,0 +1,10 @@
+let vel = 60
+
+console.log(`A velicidade do seu carro é ${vel}km/h`)
+
+if(vel > 60){
+
+    console.log(`Você ultrapassou a velocidade... MULTADO!`)
+}
+
+console.log(`Dirija com cuidado!`)
